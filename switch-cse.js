1e555ec311e4d0821c403717ec32afb1b8f8e30a
arfanovi <arfanovi.gub1@gmail.com>
2024-05-26 03:53:36 +0600

    jvascript adding

---
+++ switch-cse.js
@@ -0,0 +1,106 @@
+
+
+function changeColor () {
+    const colorSelect  = document.getElementById('colorSelect');
+    const colorBox = document.getElementById('colorBoc');
+    const selectorColor = colorSelect.value;
+
+
+switch (selectorColor) {
+    case 'Red' :
+    colorBox.style.backgroundColor = '#FF0000';
+    break;
+    case 'Green' :
+    colorBox.style.backgroundColor = '#00FF00';
+    break;
+    case 'Blue' :
+    colorBox.style.backgroundColor = '#0000FF';
+    break;
+    case 'Yellow' :
+    colorBox.style.backgroundColor = '#FFFF00';
+    break;
+    case 'Cyan' :
+    colorBox.style.backgroundColor = '#00FFFF';
+    break;
+    case 'Purple' :
+    colorBox.style.backgroundColor = '#800080';
+    break;
+    case 'Magenta' :
+    colorBox.style.backgroundColor = '#FF00FF';
+    break;
+    case 'Orange' :
+    colorBox.style.backgroundColor = '#FFA500';
+    break;
+    case 'Pink' :
+    colorBox.style.backgroundColor = '#FFC0CB';
+    break;
+    case 'Brown' :
+    colorBox.style.backgroundColor = '#A52A2A';
+    break;
+    case 'Gray' :
+    colorBox.style.backgroundColor = '#808080';
+    break;
+    case 'Black' :
+    colorBox.style.backgroundColor = '#000000';
+    break;
+    case 'White' :
+    colorBox.style.backgroundColor = '#FFFFFF';
+    break;
+    case 'Teal' :
+    colorBox.style.backgroundColor = '#008080';
+    break;
+    case 'Navy' :
+    colorBox.style.backgroundColor = '#000080';
+    break;
+    case 'Olive' :
+    colorBox.style.backgroundColor = '#808000';
+    break;
+    case 'Maroon' :
+    colorBox.style.backgroundColor = '#800000';
+    break;
+    case 'Silver' :
+    colorBox.style.backgroundColor = '#C0C0C0';
+    break;
+    case 'Gold' :
+    colorBox.style.backgroundColor = '#FFD700';
+    break;
+    case 'Beige' :
+    colorBox.style.backgroundColor = '#F5F5DC';
+    break;
+    case 'Coral' :
+    colorBox.style.backgroundColor = '#FF7F50';
+    break;
+    case 'Khaki' :
+    colorBox.style.backgroundColor = '#F0E68C';
+    break;
+    case 'Lavender' :
+    colorBox.style.backgroundColor = '#E6E6FA';
+    break;
+    case 'Salmon' :
+    colorBox.style.backgroundColor = '#FA8072';
+    break;
+    case 'Turquoise' :
+    colorBox.style.backgroundColor = '#40E0D0';
+    break;
+    case 'Violet' :
+    colorBox.style.backgroundColor = '#EE82EE';
+    break;
+    case 'Indigo' :
+    colorBox.style.backgroundColor = '#4B0082';
+    break;
+    case 'Crimson' :
+    colorBox.style.backgroundColor = '#DC143C';
+    break;
+    case 'Chocolate' :
+    colorBox.style.backgroundColor = '#D2691E';
+    break;
+    case 'SkyBlue:' :
+    colorBox.style.backgroundColor = '#FF0000';
+    break;
+    case 'RosePink' :
+    colorBox.style.backgroundColor = '#FF66CC';
+    break;
+
+
+
+}}
